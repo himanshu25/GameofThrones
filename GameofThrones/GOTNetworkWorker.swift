d9a4dff0335d8ae8d915c4c0714f73d38ec8c4eb
Himanshu <himanshu@Himanshus-MacBook-Pro.local>
2018-05-06 10:50:01 +0530

    Preparing GOT Network worker

--- GameofThrones/GOTNetworkWorker.swift
+++ GameofThrones/GOTNetworkWorker.swift
@@ -7,3 +7,34 @@
 //
 
 import Foundation
+
+class GOTNetworkWorker {
+    var infoArray = [GOTBattle]()
+    public typealias GOTInfoCompletionBlock = (NSError?, [GOTBattle]?) -> Void
+    
+    func getGotBattleDetail(with completion: @escaping GOTInfoCompletionBlock) {
+        let config = URLSessionConfiguration.default
+        var session = URLSession(configuration: config)
+        let url: URL = URL(string: "http://starlord.hackerearth.com/gotjson")!
+        let request = URLRequest(url: url)
+        
+        let dataTask = session.dataTask(with: request) { (data, response, error) in
+            if let data = data {
+                do {
+                    let dict = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.allowFragments)
+                    let dict1 = dict as? [[String: AnyObject]]
+                    print(dict1!)
+                    for  info in dict1! {
+//                        let got = GOTBattle(battleInfo: info)
+//                        self.infoArray.append(got)
+                        completion(nil, self.infoArray)
+                    }
+                    
+                } catch let error as NSError {
+                    print(error.localizedDescription)
+                }
+            }
+        }
+        dataTask.resume()
+    }
+}
