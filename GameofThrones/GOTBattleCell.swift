946cc10f3df1e4a81ffd6ee1c153a05099737cac
Himanshu <himanshu@Himanshus-MacBook-Pro.local>
2018-05-06 10:49:07 +0530

    Preparing GOTBattle Cell

--- GameofThrones/GOTBattleCell.swift
+++ GameofThrones/GOTBattleCell.swift
@@ -7,3 +7,41 @@
 //
 
 import Foundation
+import UIKit
+
+class GOTBattleCell: UITableViewCell {
+    @IBOutlet weak var headerImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var rankLabel: UILabel!
+    var gotInfo: GOTBattle!
+    
+    
+    required init(coder aDecoder: NSCoder!) {
+        super.init(coder: aDecoder)!
+    }
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String!) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        // setupUI(info: gotInfo)
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // setupUI(info: gotInfo)
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+    }
+    
+    // ViewModel
+    public func setupUI(info: GOTBattle) {
+        gotInfo = info
+        nameLabel.text = gotInfo.attackerKing
+        rankLabel.text = "\(gotInfo.rank!)"
+    }
+    func fhjfdfd () {
+        
+    }
+}
