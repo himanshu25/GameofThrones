a2116d7a1e81550b979af51737d54401701ab32a
Himanshu <himanshu@Himanshus-MacBook-Pro.local>
2018-05-06 10:48:31 +0530

    Adding businees logic

--- GameofThrones/GOTRankCalculator.swift
+++ GameofThrones/GOTRankCalculator.swift
@@ -7,3 +7,32 @@
 //
 
 import Foundation
+
+class GOTRankCalculator {
+    let battle: GOTBattle!
+    
+    // shared instance    ..... havve GOT battle object itself
+    init(_ battle: GOTBattle) {
+        self.battle = battle
+        calculateRank(attackerOutCome: battle.attackerOutcome)
+    }
+    
+    func calculateRank(attackerOutCome: String) -> Int {
+        switch attackerOutCome {
+        case "win":
+            return calculateRankForWinner()
+        case "Loose":
+            return calculateRankForLooser()
+        default: return 0
+        }
+    }
+    
+    private func calculateRankForWinner() -> Int {
+        return battle.attackerSize * battle.defenderSize
+    }
+    
+    private func calculateRankForLooser() -> Int {
+        return battle.attackerSize * battle.defenderSize
+    }
+    
+}
